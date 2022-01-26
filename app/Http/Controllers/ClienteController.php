2f12550e5dff1fc45cf9095743cb67930a5633a6
alejosb13 <88404850+alejoRegxi@users.noreply.github.com>
2022-01-25 22:14:21 -0300

    Cliente Seccion

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -11,11 +11,25 @@ class ClienteController extends Controller
     /**
      * Display a listing of the resource.
      *
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $response = [];
+        $status = 200;
+        $clienteEstado = 1; // Activo
+        
+        if($request->input()) $clienteEstado = $request->input("estado");
+        
+        // dd($clienteEstado);
+        $cliente =  Cliente::where('estado',$clienteEstado)->get();
+        
+        if(count($cliente) > 0){
+            $response[] = $cliente;
+        }
+        
+        return response()->json($cliente, $status);
     }
 
     /**
@@ -41,7 +55,7 @@ class ClienteController extends Controller
             'frecuencia_id' => 'required|numeric',
             'nombre' => 'required|string|max:80',
             'celular' => 'required|numeric',
-            'telefono' => 'required|numeric',
+            'telefono' => 'required|numeric|unique:clientes,telefono',
             'direccion_casa' => 'required|string|max:180',
             'direccion_negocio' => 'required|string|max:180',
             'cedula' => 'required|string|max:22',
@@ -70,26 +84,52 @@ class ClienteController extends Controller
             ]);
             
             return response()->json([
-                'success' => 'Usuario Insertado con exito',
-                'data' =>[
+                // 'success' => 'Usuario Insertado con exito',
+                // 'data' =>[
                     'id' => $user->id,
-                ]
+                // ]
             ], 201);
         }
         
-
-
     }
 
     /**
      * Display the specified resource.
-     *
+     * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show($id,Request $request)
     {
-        //
+        $response = [];
+        $status = 400;
+        $clienteEstado = 1; // Activo
+        
+        if(is_numeric($id)){
+                    
+            if($request->input()) $clienteEstado = $request->input("estado");
+        
+            // dd($clienteEstado);
+            $cliente =  Cliente::where([
+                ['id', '=', $id],
+                ['estado', '=', $clienteEstado],
+            ])->first();
+        
+        
+            // $cliente =  Cliente::find($id);
+            if($cliente){
+                $response = $cliente;
+                $status = 200;
+
+            }else{
+                $response[] = "El cliente no existe o fue eliminado.";
+            }
+            
+        }else{
+            $response[] = "El Valor de Id debe ser numerico.";
+        }
+        
+        return response()->json($response, $status);
     }
 
     /**
@@ -112,7 +152,66 @@ class ClienteController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $response = [];
+        $status = 400;
+        
+        if(is_numeric($id)){
+            $cliente =  Cliente::find($id);
+            
+            if($cliente){ 
+                $validation = Validator::make($request->all() ,[
+                    'categoria_id' => 'required|numeric',
+                    'frecuencia_id' => 'required|numeric',
+                    'nombre' => 'required|string|max:80',
+                    'celular' => 'required|numeric',
+                    'telefono' => 'required|numeric',
+                    'direccion_casa' => 'required|string|max:180',
+                    'direccion_negocio' => 'required|string|max:180',
+                    'cedula' => 'required|string|max:22',
+                    'dias_cobro' => 'required|string|max:20',
+                    'fecha_vencimiento' => 'required|date',
+                    'estado' => 'required|numeric|max:1',
+                ]);
+                
+                if($validation->fails()) {
+                    $response[] = $validation->errors();
+                } else {
+
+                    
+                    $clienteUpdate = $cliente->update([
+                        'categoria_id' => $request['categoria_id'],
+                        'frecuencia_id' => $request['frecuencia_id'],
+                        'nombre' => $request['nombre'],
+                        'celular' => $request['celular'],
+                        'telefono' => $request['telefono'],
+                        'direccion_casa' => $request['direccion_casa'],
+                        'direccion_negocio' => $request['direccion_negocio'],
+                        'cedula' => $request['cedula'],
+                        'dias_cobro' => $request['dias_cobro'],
+                        'fecha_vencimiento' => $request['fecha_vencimiento'],
+                        'estado' => $request['estado'],
+                    ]);
+
+                    
+                    if($clienteUpdate){                  
+                        $response[] = 'Cliente modificado con exito.';
+                        $status = 200;
+                        
+                    }else{
+                        $response[] = 'Error al modificar los datos.';
+                    }
+
+                }
+
+            }else{
+                $response[] = "El cliente no existe.";
+            }
+            
+        }else{
+            $response[] = "El Valor de Id debe ser numerico.";
+        }
+        
+        return response()->json($response, $status);
     }
 
     /**
@@ -123,6 +222,33 @@ class ClienteController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $response = [];
+        $status = 400;
+        
+        if(is_numeric($id)){
+            $cliente =  Cliente::find($id);
+            
+            if($cliente){ 
+                $clienteDelete = $cliente->update([
+                    'estado' => 0,
+                ]);
+                
+                if($clienteDelete){                  
+                    $response[] = 'cliente fue eliminado con exito.';
+                    $status = 200;
+                    
+                }else{
+                    $response[] = 'Error al eliminar el cliente.';
+                }
+
+            }else{
+                $response[] = "El cliente no existe.";
+            }
+            
+        }else{
+            $response[] = "El Valor de Id debe ser numerico.";
+        }
+        
+        return response()->json($response, $status);
     }
 }
