24f0cface0dceb8856a99a6f2f4be65b5fafcb5a
Alejandro.Sanchez <alejandro.sanchez@comafi.com.ar>
2022-10-29 16:20:29 -0300

    recuperacion

---
+++ database/migrations/2022_10_24_172338_create_meta_recuperacions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMetaRecuperacionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meta_recuperacions', function (Blueprint $table) {
+            $table->id();
+
+            // usuarios
+            $table->unsignedBigInteger("user_id");
+            $table->foreign("user_id")->references("id")->on("users");
+
+            $table->double('monto_meta', 7, 2);
+
+            $table->integer("estado")->length(1);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('meta_recuperacions');
+    }
+}
